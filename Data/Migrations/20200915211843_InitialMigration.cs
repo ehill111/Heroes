fbce2451580c522c73cba679cffbfd22553f6ccd
Eric M Hill <ehill111@comcast.net>
2020-09-16 08:46:40 -0400

    First commit

---
+++ Data/Migrations/20200915211843_InitialMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperHeroProject.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hero",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    heroName = table.Column<string>(nullable: true),
+                    aliasFirstName = table.Column<string>(nullable: true),
+                    aliasLastname = table.Column<string>(nullable: true),
+                    primaryAbility = table.Column<string>(nullable: true),
+                    secondaryAbility = table.Column<string>(nullable: true),
+                    catchPhrase = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hero", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hero");
+        }
+    }
+}
---
+++ Models/Heroes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroProject.Models
+{
+    public class Heroes
+    {
+        [Key]
+        public int Id { get; set; }
+        public string heroName { get; set; }
+        public string aliasFirstName { get; set; }
+        public string aliasLastname { get; set; }
+        public string primaryAbility { get; set; }
+        public string secondaryAbility { get; set; }
+        public string catchPhrase { get; set; }
+    }
+}
